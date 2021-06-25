869ee4ed57eb237f770102f66e7586361634def4
Claudio Ortega <claudio.rtega2701@gmail.com>
2021-06-25 17:13:12 -0500

    fix model namae

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def darcolor(doc):
 def load_model(name):
     nlp=spacy.load(name)
     return nlp
-nlp = load_model('models1')
+nlp = load_model('es_core_news_md')
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
 LANGUAGE = "spanish"
 #leer archivo
